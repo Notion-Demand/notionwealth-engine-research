6fbd796706d017b8322c1337944c62cb31988b0f
raunaksharan <raunak.sharan01@gmail.com>
2026-07-10 21:12:15 +0530

    chore: remove debug scripts, add tsx as devDependency

--- scripts/_debug-tcp-test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Testing TCP connectivity to quantalyze-postgres.postgres.database.azure.com:5432 ..."
-timeout 8 bash -c "echo > /dev/tcp/quantalyze-postgres.postgres.database.azure.com/5432" 2>&1
-echo "Result: $?"
-echo "---"
-echo "DNS resolution check:"
-getent hosts quantalyze-postgres.postgres.database.azure.com 2>&1 || echo "DNS resolution FAILED"
